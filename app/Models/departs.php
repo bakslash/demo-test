1f2d4d643ac77d83d5460f60288419ef3eaadbab
bakslash <marcusmuasa@gmail.com>
2024-01-03 16:02:34 +0300

    updates

--- app/Models/departs.php
+++ app/Models/departs.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class departments extends Model
+class departs extends Model
 {
     use HasFactory;
+    protected $fillable = ['code', 'name'];
 }
